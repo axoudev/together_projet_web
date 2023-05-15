ac7ebba85563449c54274d2dc5772d9d6c6a6048
Axeldev <axl.bertrand.webdev@gmail.com>
2023-05-16 00:53:58 +0200

    modification landing page

---
+++ public/build/assets/Dashboard-0930a6a8.js
@@ -0,0 +1,1 @@
+import{c as r,o as a}from"./app-6be21289.js";import o from"./Welcome-5753e29c.js";const p={__name:"Dashboard",props:{activities:Array},setup(t){return(c,e)=>(a(),r(o))}};export{p as default};
